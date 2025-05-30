906c89069805d9eacc0cb92de9e8294d83d1b77f
Oriol Ripalta Maso <oriolripalta@hotmail.com>
2025-05-30 18:27:18 -0400

    factors

--- src/DataCollector.py
+++ src/DataCollector.py
@@ -1,11 +1,26 @@
-import yfinance
-
+import pandas as pd
+import yfinance as yf
 class DataCollector:
-    def __init__(self, tickers):
+    def __init__(self, tickers, beg, end):
         self.tickers = tickers
+        self.beg = beg
+        self.end = end
 
+    def get_prices(self):
+        data = None
+        if self.beg and self.end:
+            data = yf.download(self.tickers, start = self.beg, end = self.end, auto_adjust=True)
+        else:
+            data = yf.download(self.tickers, period = "5y", interval = "1d", auto_adjust=True)
+        data = data['Close'] if data else None
+        return data
+    
     def get_returns(self):
-        data = yfinance.download(self.tickers, period = "5y", interval = "1d")
-        data = data['Close']
-        data = data.pct_change().dropna()
+        data = None
+        if self.beg and self.end:
+            data = yf.download(self.tickers, start = self.beg, end = self.end, auto_adjust=True)
+        else:
+            data = yf.download(self.tickers, period = "5y", interval = "1d", auto_adjust=True)
+        data = data['Close'] if isinstance(data, pd.DataFrame) else None
+        data = data.pct_change().dropna() if isinstance(data, pd.DataFrame) else None
         return data
